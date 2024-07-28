a951d88976acb3da43e012f94d1d9908ad4bf93b
makatastrophe <martin.amiens@gmail.com>
2024-07-28 21:29:59 +0200

    essai 2

--- test-neptune/simple_logging.py
+++ test-neptune/simple_logging.py
@@ -1,36 +1,16 @@
 import neptune
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
 
-# Initialize a Neptune run
 run = neptune.init_run(
     project="martin-amiens/HAL-data-names-errors-findind",
-    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIxNDZiMTJjYy02MmEyLTRiNGUtODc5Ny1lNDgxM2EwOTY2YjcifQ"
-)
+    api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIxNDZiMTJjYy02MmEyLTRiNGUtODc5Ny1lNDgxM2EwOTY2YjcifQ==",
+)  # your credentials
 
-# Load a dataset
-data = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
-
-# Define and log model parameters
-params = {"n_estimators": 100, "max_depth": 2, "random_state": 42}
+params = {"learning_rate": 0.001, "optimizer": "Adam"}
 run["parameters"] = params
 
-# Train a simple model
-model = RandomForestClassifier(**params)
-model.fit(X_train, y_train)
+for epoch in range(10):
+    run["train/loss"].append(0.9 ** epoch)
 
-# Log training and evaluation metrics
-train_preds = model.predict(X_train)
-test_preds = model.predict(X_test)
+run["eval/f1_score"] = 0.66
 
-train_f1 = f1_score(y_train, train_preds, average="macro")
-test_f1 = f1_score(y_test, test_preds, average="macro")
-
-run["train/f1_score"] = train_f1
-run["eval/f1_score"] = test_f1
-
-# Stop the run
 run.stop()
